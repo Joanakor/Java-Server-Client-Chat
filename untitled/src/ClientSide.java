53ad25fa4de7741a80ff295df84532dcb56a7a7a
Joanakor <mark67769@gmail.com>
2023-03-31 16:12:51 +0300

    One message chat is done

--- untitled/src/ClientSide.java
+++ untitled/src/ClientSide.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientSide {
@@ -6,6 +6,16 @@ public class ClientSide {
         System.out.println("Client started!");
         try {
             Socket client = new Socket("localhost", 3947);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Send a message to the server: ");
+            String input = userInput.readLine();
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            out.println(input);
+
+            BufferedReader get = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String answer = get.readLine();
+            System.out.println(answer);
+            client.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- untitled/src/ServerSide.java
+++ untitled/src/ServerSide.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,6 +12,17 @@ public class ServerSide {
             ServerSocket serverSocket = new ServerSocket(3947);
             Socket server = serverSocket.accept();
             System.out.println("Connection established!");
+            BufferedReader get = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            String answer = get.readLine();
+            System.out.println(answer);
+
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Send a message to the client: ");
+            String input = userInput.readLine();
+            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+            out.println(input);
+            server.close();
+            serverSocket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
